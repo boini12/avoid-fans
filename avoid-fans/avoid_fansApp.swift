c09553f29f58249bf662e1c7d532d2e81dfd265e
Ines Bohr <inesmaria.bohr@st.oth-regensburg.de>
2023-08-25 13:23:53 +0200

    Initial Commit

---
+++ avoid-fans/avoid_fansApp.swift
@@ -0,0 +1,17 @@
+//
+//  avoid_fansApp.swift
+//  avoid-fans
+//
+//  Created by Ines on 25.08.23.
+//
+
+import SwiftUI
+
+@main
+struct avoid_fansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
