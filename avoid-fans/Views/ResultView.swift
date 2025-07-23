a9ee69869fef220e9bb1056f6635fd2fe63dc193
Ines Bohr <bohrines@gmail.com>
2025-07-23 19:43:19 +0200

    Added basic navigation to ResultView

---
+++ avoid-fans/Views/ResultView.swift
@@ -0,0 +1,14 @@
+//
+//  ResultView.swift
+//  avoid-fans
+//
+//  Created by Ines Bohr on 23.07.25.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
--- avoid-fans/Views/UserInputView.swift
+++ avoid-fans/Views/UserInputView.swift
@@ -17,54 +17,60 @@ struct UserInputView: View {
     @State private var validStations = true
     @State private var selectedOriginIndex =  0
     @State private var selectedDestinationIndex = 0
+    @State private var navigationPath = NavigationPath()
     
     private var apiManager = APIManager()
     
     var body: some View {
-        VStack{
-            HStack
-            {
-                Spacer(minLength: 0)
-                // Origin
-                PickerView(stations: viewModel.getStations(), selectedOptionIndex: $selectedOriginIndex)
-                Spacer(minLength: 0)
-            }
-            .padding()
-            HStack
-            {
-                Spacer(minLength: 0)
-                // Destination
-                PickerView(stations: viewModel.getStations(), selectedOptionIndex: $selectedDestinationIndex)
-                Spacer(minLength: 0)
-            }
-            
-            // Start date
-            DatePicker("Start date",
-                    selection: $startDate, displayedComponents: [.date, .hourAndMinute])
-            .padding()
-            
-            // End date
-            DatePicker("End date",
-                    selection: $endDate, displayedComponents: [.date, .hourAndMinute])
-            .padding()
-            
-            Button("Check for crazy fans")
-            {
-                let userInput = UserInput(startDate: startDate, endDate: endDate, origin: viewModel.getStations()[selectedOriginIndex], destination: viewModel.getStations()[selectedDestinationIndex])
-                viewModel.validate(input: userInput)
-            }.errorAlert(error: $viewModel.error)
-            .padding()
-            
-            // Run an async task to make a request to the API
-            .task {
-                do{
-                    let matches =  try await apiManager.fetchBundesligaMatches()
-                    let controller = APIDataController(matches: matches)
-                    let result = controller.checkForMatches(startDate: startDate, endDate: endDate)
-                    print(result)
-                }catch{
-                    // do nothing for now
+        NavigationStack(path: $navigationPath) {
+            VStack{
+                HStack
+                {
+                    Spacer(minLength: 0)
+                    // Origin
+                    PickerView(stations: viewModel.getStations(), selectedOptionIndex: $selectedOriginIndex)
+                    Spacer(minLength: 0)
                 }
+                .padding()
+                HStack
+                {
+                    Spacer(minLength: 0)
+                    // Destination
+                    PickerView(stations: viewModel.getStations(), selectedOptionIndex: $selectedDestinationIndex)
+                    Spacer(minLength: 0)
+                }
+                
+                // Start date
+                DatePicker("Start date",
+                        selection: $startDate, displayedComponents: [.date, .hourAndMinute])
+                .padding()
+                
+                // End date
+                DatePicker("End date",
+                        selection: $endDate, displayedComponents: [.date, .hourAndMinute])
+                .padding()
+                
+                Button("Check for crazy fans")
+                {
+                    let userInput = UserInput(startDate: startDate, endDate: endDate, origin: viewModel.getStations()[selectedOriginIndex], destination: viewModel.getStations()[selectedDestinationIndex])
+                    viewModel.validate(input: userInput)
+                    navigationPath.append("Test")
+                }.errorAlert(error: $viewModel.error)
+                .padding()
+                
+                // Run an async task to make a request to the API
+                .task {
+                    do{
+                        let matches =  try await apiManager.fetchBundesligaMatches()
+                        let controller = APIDataController(matches: matches)
+                        let result = controller.checkForMatches(startDate: startDate, endDate: endDate)
+                        print(result)
+                    }catch{
+                        // do nothing for now
+                    }
+                }
+            }.navigationDestination(for: String.self) { result in
+                ResultView()
             }
         }
     }
